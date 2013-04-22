efe083c2c44efd6f3f59f3cbc32c51397a9b2e72
alpdeniz <alpdeniz@gmail.com>
2013-04-22 08:09:08 +0200

    filesystembase'den tanimli static ve templateler

--- evmev/settings.py
+++ evmev/settings.py
@@ -50,18 +50,18 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = '/static/img/'
+MEDIA_ROOT = '/app/static/img/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
 MEDIA_URL = ''
-ADMIN_MEDIA_PREFIX = '/static/admin/'
+ADMIN_MEDIA_PREFIX = '/app/static/admin/'
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/static/'
+STATIC_ROOT = '/app/static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -69,7 +69,10 @@ STATIC_URL = '/static/'
 
 # Additional locations of static filesACCOUNT_ACTIVATION_DAYS = 7
 STATICFILES_DIRS = (
-    
+    '/app/static/img/'
+	'/app/static/admin/'
+	'/app/static/uploader/'
+	'/app/static/uni_form/'
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
